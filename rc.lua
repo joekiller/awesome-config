599da404ee9a855bef15975342350067e7daddc6
Joseph Lawson <joe@joekiller.com>
2017-05-04 11:13:48 -0400

    load arch theme

--- rc.lua
+++ rc.lua
@@ -102,7 +102,11 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
+themesdir = "/home/jlawson/.config/awesome/themes/"
+themename = "arch"
+themedir  = themesdir .. themename
+themepath = themedir .. "/theme.lua"
+beautiful.init(themepath)
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
